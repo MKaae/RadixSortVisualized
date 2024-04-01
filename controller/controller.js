07de270c87f914d3bf33c1d3c5c3327e5ab5fac9
zivbabs <117026503+zivbabs@users.noreply.github.com>
2024-04-01 15:44:23 +0200

    adds view for autofunction, and radix sort

---
+++ controller/controller.js
@@ -0,0 +1,77 @@
+"use strict";
+import { populateBoard } from "../view/view.js"
+
+const testArr = [0, 1, 382, 32, 49, 20, 2133, 34825, 123, 9, 4294]; // Array that will be sorted.
+let iterations = []; // This contains every iteration of the algorithm so we can visualise it later on.
+let timeOutValue = 300; // This controls how fast our tickrate is.
+
+export function startRadixSort() {
+    radixSort(testArr);
+}
+
+export function getMaxDigit() { // This returns the length of the longest number in the array. It will control how many iterations we will run.
+    return mostDigits(testArr);
+}
+
+function getDigit(num, place) {
+    return Math.floor(Math.abs(num) / Math.pow(10, place)) % 10; //Math.abs make sure its always positive number, entire algo makes it count digits from back in arraystyle.
+}
+
+function digitCount(num) {
+    if (num === 0) return 1;
+    return Math.floor(Math.log10(Math.abs(num))) + 1; //Calculates the amount of times it is uplifted with 10 number logaritme.
+}
+
+function mostDigits(nums) {
+    let maxDigits = 0;
+
+    for (let i = 0; i < nums.length; i++) {
+        maxDigits = Math.max(maxDigits, digitCount(nums[i])); //Checks the array for the highest possible length of a number so we count from correct position.
+    }
+
+    return maxDigits;
+}
+
+function radixSort(arrOfNums) {
+    let maxDigitCount = mostDigits(arrOfNums);
+
+    for (let k = 0; k < maxDigitCount; k++) {
+        // Outerloops runs as many times as the largest number has digits
+        let digitBuckets = Array.from({ length: 10 }, () => []); // creates a new array of buckets
+        for (let i = 0; i < arrOfNums.length; i++) {
+            let digit = getDigit(arrOfNums[i], k); // Get the digit to evaluate
+            digitBuckets[digit].push(arrOfNums[i]); // pushes the number to the correct bucket
+        }
+        iterations.push(digitBuckets);
+        arrOfNums = [].concat(...digitBuckets); // flattens the buckets into a new arraylist
+    }
+
+    return arrOfNums; // returns the finished array
+}
+
+export function getArray() { // This returns the array to the view
+    return testArr;
+}
+
+export function autoBoard() {
+    startRadixSort(); //Sorts the test array, we dont use it just  for show.
+    for (let i = 0; i < iterations.length; i++) {
+        const buckets = iterations[i]; //All the buckets in the current iteration
+
+        for (let j = 0; j < buckets.length; j++) {
+            const bucketArray = buckets[j] //The array within that bucket
+
+            for (let k = 0; k < bucketArray.length; k++) {
+                const value = bucketArray[k]; //The value within that bucket array
+                setTimeout(() => {
+                    populateBoard(value, j, i, testArr.length, iterations.length); //Timeout for animating the view
+                }, timeOutValue);
+                timeOutValue += 300;
+            }
+        }
+    }
+}
+
+export function clickBoard() {
+
+}
--- view/view.js
+++ view/view.js
@@ -1,1 +1,77 @@
 "use strict"
+
+import { getMaxDigit, getArray, clickBoard, autoBoard } from "../controller/controller.js"
+
+buttonListeners();
+
+let newDigit = 0;
+
+function buttonListeners() {
+    document.getElementById('btn-start').addEventListener('click', () => startView());
+}
+function startView() {
+    document.getElementById('btn-start').classList.add("hidden");
+    const btnMax = document.getElementById('btn-max');
+    const currentArray = getArray();
+    document.getElementById('current-array').textContent = `[${currentArray}]`;
+
+    btnMax.classList.remove("hidden");
+    btnMax.addEventListener('click', () => getMaxNumber());
+}
+
+function getMaxNumber() {
+    document.getElementById('btn-max').classList.add("hidden");
+    const maxDigit = document.getElementById('max-digit');
+    newDigit = getMaxDigit();
+
+    maxDigit.textContent = `Max digit = ${newDigit}`;
+    const btnSort = document.getElementById("btn-sort");
+    btnSort.classList.remove("hidden");
+    document.getElementById('btn-sort').addEventListener('click', () => viewBoard());
+}
+
+function viewBoard() {
+    const board = document.getElementById("board");
+
+    board.classList.remove("hidden");
+    document.getElementById('btn-play').addEventListener('click', () => clickUpdateView());
+    document.getElementById('btn-auto').addEventListener('click', () => autoUpdateBoardView());
+}
+
+function clickUpdateView() {
+    clickBoard();
+}
+
+function autoUpdateBoardView() {
+    autoBoard();
+}
+
+let lastNumber;
+let tempArray = [];
+
+export function populateBoard(value, bucket, iteration, arrayLength, iterationsLength) {
+    lastNumber = value;
+    tempArray.push(lastNumber);
+
+    const divNumber = document.getElementById(`b-${bucket}`).firstElementChild; // Get a hold of the numbers div in buckets
+    let divHTML = divNumber.innerHTML;
+    divHTML += `<div id="number">${value}</div>`;
+
+    divNumber.innerHTML = divHTML;
+
+    if (tempArray.length === arrayLength) { //Every 10th iteration we display the new array
+        displayNewArray(tempArray);
+        tempArray = [];
+        document.getElementById('current-iteration').textContent = `Current iteration: ${iteration + 1}`
+
+        if (iteration !== iterationsLength - 1) { //We clear the elements everytime we have iterated through all elements, except the last iteration
+            for (let i = 0; i < 10; i++) {
+                document.getElementById(`b-${i}`).firstElementChild.innerHTML = "";
+            }
+        }
+    }
+}
+
+export function displayNewArray(tempArray) {
+    document.getElementById('new-array').textContent = `[${tempArray}]`
+}
